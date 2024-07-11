ad96d4c773e25687166ddce5d992140957f2fa76
Venoth <venoth_anandan@trimble.com>
2024-07-11 22:50:35 +0530

    update emi logic - completed

--- src/App.js
+++ src/App.js
@@ -1,60 +1,102 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { numberWithCommas } from "./utils/config";
 import './App.css';
+import TextInput from "./components/text-input";
 import SliderInput from "./components/slider-input";
 import {tenureData} from './utils/constants'
 
 function App() {
-  const [cost, SetCost] = useState(0);
-  const [interest, SetInterest] = useState(0);
-  const [fee, SetFee] = useState(0);
-  const [downPayment,setDownPayment] = useState(0);
-  const [emi,setEmi] = useState(0);
+  
+  const [cost, setCost] = useState(0);
+  const [interest, setInterest] = useState(10);
+  const [fee, setFee] = useState(1);
+  const [downPayment, setDownPayment] = useState(0);
   const [tenure, setTenure] = useState(12);
+  const [emi, setEmi] = useState(0);
 
+  const calculateEMI = (downpayment) => {
+    // EMI amount = [P x R x (1+R)^N]/[(1+R)^N-1]
+    if (!cost) return;
 
-  const updateDownPayment = (event) => {
-      if(!cost){
-        return;
-      }
-      const dp = Number(event.target.value);
-      setDownPayment(dp.toFixed(0));
-      //calculate downpaymen
+    const loanAmt = cost - downpayment;
+    const rateOfInterest = interest / 100;
+    const numOfYears = tenure / 12;
+
+    const EMI =
+      (loanAmt * rateOfInterest * (1 + rateOfInterest) ** numOfYears) /
+      ((1 + rateOfInterest) ** numOfYears - 1);
+
+    return Number(EMI / 12).toFixed(0);
   };
-  const updateEMI = (event) => {
-    if(!cost){
-      return;
+
+  const calculateDP = (emi) => {
+    if (!cost) return;
+
+    const downPaymentPercent = 100 - (emi / calculateEMI(0)) * 100;
+    return Number((downPaymentPercent / 100) * cost).toFixed(0);
+  };
+
+  const updateEMI = (e) => {
+    if (!cost) return;
+
+    const dp = Number(e.target.value);
+    setDownPayment(dp.toFixed(0));
+
+    const emi = calculateEMI(dp);
+    setEmi(emi);
+  };
+  const updateDownPayment = (e) => {
+    if (!cost) return;
+
+    const emi = Number(e.target.value);
+    setEmi(emi.toFixed(0));
+
+    const dp = calculateDP(emi);
+    setDownPayment(dp);
+  };
+
+  const totalDownPayment = () => {
+    return numberWithCommas(
+      (Number(downPayment) + (cost - downPayment) * (fee / 100)).toFixed(0)
+    );
+  };
+
+  const totalEMI = () => {
+    return numberWithCommas((emi * tenure).toFixed(0));
+  };
+
+
+  useEffect(() => {
+    if (!(cost > 0)) {
+      setDownPayment(0);
+      setEmi(0);
     }
-    const mp = Number(event.target.value); //monthly payment
-    setEmi(mp.toFixed(0));
-    //calc emi
-  };
-  const totalDownPayment = () => {};
-  const totalEMI = () => {};
-  const calculateEMI = (downPayment) => {};
+
+    const emi = calculateEMI(downPayment);
+    setEmi(emi);
+  }, [tenure, cost]);
 
   return (
     <div class="App">
-      <span class="title">EMI Calculator</span>
-      <span class="title">Total cost of Asset</span>
-      <input
-        type="number"
-        value={cost}
-        onChange={(e) => SetCost(e.target.value)}
-        placeholder="Total cost of Asset"
+        <span className="title" style={{ fontSize: 30, marginTop: 10 }}>
+        EMI Calculator
+      </span>
+      <TextInput
+        title={"Total Cost of Asset"}
+        state={cost}
+        setState={setCost}
       />
-      <span class="title">Interest rate in Fee</span>
-      <input
-        type="number"
-        value={cost}
-        onChange={(e) => SetCost(e.target.value)}
-        placeholder="Total cost of Asset"
+
+      <TextInput
+        title={"Interest Rate (in %)"}
+        state={interest}
+        setState={setInterest}
       />
-      <span class="title">Processing rate in Fee</span>
-      <input
-        type="number"
-        value={cost}
-        onChange={(e) => SetCost(e.target.value)}
-        placeholder="Total cost of Asset"
+
+      <TextInput
+        title={"Processing Fee (in %)"}
+        state={fee}
+        setState={setFee}
       />
       <SliderInput
         title="Down Payment"
